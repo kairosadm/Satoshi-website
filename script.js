14d9b875b0967a239b547c12ff87bf8c780c5da2
kairos <56596729+kairosadm@users.noreply.github.com>
2022-02-28 14:58:37 +0100

    Created script.js
    JavaScript for a little quiz (only 2 questions but a show answers button)

---
+++ script.js
@@ -0,0 +1,133 @@
+.titre1 {
+    font-family: "Arial";
+    color: rgba(0, 0, 0, 1);
+    opacity: 1;
+    position: relative;
+    font-size: 3vw;
+}
+.texte1 {
+    font-family: "Arial";
+    color: rgba(0, 0, 0, 1);
+    margin-left: 50px;
+    opacity: 1;
+}
+
+.titre2 {
+    font-family: "Arial";
+    color: rgba(0, 0, 0, 1);
+    opacity: 1;
+}
+
+.row {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+    background-color: #fce5cd;
+}
+
+.img-fluid {
+    margin-left: 200px;
+    max-width: 64.5%;
+    margin-top: 30px;
+    height: auto;
+    margin-bottom: 30px;
+}
+
+.col {
+    -ms-flex-preffered-size: 0;
+    flex-basis: 0;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    max-width: 100%;
+}
+
+.img-fluid2 {
+    height: auto;
+    position: relative;
+    margin-left: 30px;
+    margin-bottom: 50px;
+}
+html,
+body {
+    scroll-behavior: smooth;
+}
+
+a:visited {
+    color: rgb(51, 59, 175);
+}
+
+a:hover {
+    color: rgb(26, 4, 124);
+    text-decoration: underline;
+    font-weight: 450;
+}
+
+#navbar {
+    overflow: hidden;
+    background-color: #000;
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+}
+
+#navbar a {
+    float: left;
+    display: block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 9px;
+    font-family: "Arial";
+    text-decoration: none;
+}
+#navbar a:hover {
+    background: rgb(126, 121, 121);
+    color: black;
+}
+#main {
+    margin-top: 60px;
+}
+
+
+
+nomics-ticker-widget {
+    margin: 100px;
+}
+
+.row3{
+    font-family: "Arial"
+}
+
+.question{
+    font-weight: 600;
+    margin-left: 20px;
+}
+
+.answers {
+    margin-bottom: 20px;
+    margin-left: 20px;
+}
+
+#submit{
+  font-family: sans-serif;
+  font-size: 20px;
+  background-color: #297;
+  color: #fff;
+  border: 0px;
+  border-radius: 3px;
+  padding: 10px;
+  cursor: pointer;
+  margin-bottom: 20px;
+    margin-left: 20px;
+}
+
+#submit:hover{
+  background-color: #3a8;
+}
+
+.texte3{
+    margin-left: 20px;
+}
